fb2151692ab1eb773714bcc14318c71324855bde
rvillasevil <rvillasevil@gmail.com>
2025-11-29 11:07:45 +0100

    new users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,36 @@
 class RegistrationsController < Devise::RegistrationsController
+  def new
+    invitation_token = params[:invitation_token].to_s.strip
+
+    return super if invitation_token.blank?
+
+    invitation = ClientInvitation.find_by(token: invitation_token, accepted_at: nil)
+
+    unless invitation
+      flash[:alert] = 'La invitación es inválida o ha expirado.'
+      return redirect_to new_user_session_path
+    end
+
+    user = User.find_or_initialize_by(email: invitation.email, owner_id: invitation.owner_id)
+
+    if user.new_record?
+      password = Devise.friendly_token.first(20)
+      user.assign_attributes(role: :guest, password: password, password_confirmation: password)
+      user.save
+    end
+
+    if user.persisted?
+      invitation.update!(accepted_at: Time.current)
+      self.resource = user
+      set_flash_message! :notice, :signed_up
+      sign_in(resource_name, user)
+      redirect_to after_sign_up_path_for(user)
+    else
+      flash[:alert] = user.errors.full_messages.to_sentence
+      redirect_to new_user_session_path
+    end
+  end
+
   def create
 
     invitation_token = params[:invitation_token].to_s.strip
